2df4ec74981efd3af0e62f0d2d2b9beef37a83e3
s4edev <tom.solacroup@gmail.com>
2014-12-04 22:24:19 +0100

    reverted a bad commit

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,21 +1,5 @@
 window.onload = function()
 {
-<<<<<<< .merge_file_a09120
-	//I'm gonna set a scene!
-
-	//scene size
-	var WIDTH = 400, HEIGHT = 300;
-
-	//setting some camera attributes
-	var VIEW_ANGLE = 45,
-	ASPECT = WIDTH / HEIGHT,
-	NEAR = 0.1,
-	FAR = 10000;
-
-	//get the DOM element to attach to
-	var container = $('#container');
-=======
->>>>>>> .merge_file_a08680
 
 	//create a WebGL renderer, a camera, and a scene
     var renderer = new THREE.WebGLRenderer();
@@ -30,24 +14,6 @@ window.onload = function()
 
 	camera.position.z = 100;
 
-<<<<<<< .merge_file_a09120
-	//create the sphere's material
-	var sphereMaterial = new THREE.MeshBasicMaterial(
-		{
-			color: 0xCC0000
-		});
-
-	//create a new mesh with sphere geometry
-	var sphere = new THREE.Mesh(
-		new THREE.SphereGeometry(
-			radius,
-			segments,
-			rings),
-		sphereMaterial);
-
-	//add the sphere to the scene
-	scene.add(sphere);
-=======
     //create a new mesh with sphere geometry
     var sphereMaterial = new THREE.MeshBasicMaterial({color : 0xffffff, transparent : true, opacity : 0.5});
 	var sphere = new THREE.Mesh(new THREE.SphereGeometry(50, 64, 64), sphereMaterial);
@@ -63,5 +29,4 @@ window.onload = function()
     };
 
     render();
->>>>>>> .merge_file_a08680
 };
